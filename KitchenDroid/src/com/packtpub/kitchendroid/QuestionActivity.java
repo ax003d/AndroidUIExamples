c57606eb353db6bfbdd9e53a8a41f01d0ed31bab
Robert <rzheng@xtremeprog.com>
2012-10-31 13:06:34 +0800

    * init kitchendroid project

---
+++ KitchenDroid/src/com/packtpub/kitchendroid/QuestionActivity.java
@@ -0,0 +1,15 @@
+package com.packtpub.kitchendroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuestionActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
