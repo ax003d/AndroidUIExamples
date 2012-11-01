b607f730332b6a389c3acb4c17e9448720c9945d
Robert <rzheng@xtremeprog.com>
2012-11-01 15:10:41 +0800

    * tabhost & rating bar & text switcher

--- RoboticReview/src/com/packtpub/roboticreview/ReviewActivity.java
+++ RoboticReview/src/com/packtpub/roboticreview/ReviewActivity.java
@@ -1,15 +1,81 @@
 package com.packtpub.roboticreview;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TextSwitcher;
+import android.widget.ViewSwitcher;
 
-public class ReviewActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class ReviewActivity extends TabActivity implements
+		ViewSwitcher.ViewFactory, Runnable {
+
+	private final Handler switchCommentHandler = new Handler();
+	private TextSwitcher switcher;
+	private String[] comments;
+	private int commentIndex = 0;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		TabHost tabs = getTabHost();
+		getLayoutInflater().inflate(R.layout.main, tabs.getTabContentView(),
+				true);
+		Resources resources = getResources();
+		TabHost.TabSpec details = tabs
+				.newTabSpec("review")
+				.setContent(R.id.review)
+				.setIndicator(getString(R.string.review),
+						resources.getDrawable(R.drawable.review));
+
+		TabHost.TabSpec gallery = tabs
+				.newTabSpec("photos")
+				.setContent(R.id.photos)
+				.setIndicator(getString(R.string.gallery),
+						resources.getDrawable(R.drawable.photos));
+
+		TabHost.TabSpec reservation = tabs
+				.newTabSpec("review")
+				.setContent(R.id.reservation)
+				.setIndicator(getString(R.string.reservation),
+						resources.getDrawable(R.drawable.reservation));
+
+		tabs.addTab(details);
+		tabs.addTab(gallery);
+		tabs.addTab(reservation);
+
+		comments = resources.getStringArray(R.array.comments);
+		switcher = (TextSwitcher) findViewById(R.id.reviews);
+		switcher.setFactory(this);
+	}
+
+	public View makeView() {
+		return getLayoutInflater().inflate(R.layout.review_comment, null);
+	}
+
+	protected void onStart() {
+		super.onStart();
+		switchCommentHandler.postDelayed(this, 5 * 1000l);
+	}
+
+	protected void onStop() {
+		super.onStop();
+		switchCommentHandler.removeCallbacks(this);
+	}
+
+	public void run() {
+		try {
+			switcher.setText(comments[commentIndex++]);
+			if (commentIndex >= comments.length) {
+				commentIndex = 0;
+			}
+		} finally {
+			switchCommentHandler.postDelayed(this, 5 * 1000l);
+		}
+	}
+
 }
