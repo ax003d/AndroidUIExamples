7571140d2f42cbd1e3507fad0568277ddd470935
Robert <rzheng@xtremeprog.com>
2012-11-05 17:23:39 +0800

    * implement a list activity

--- Layouts/src/com/packtpub/layouts/LayoutSelectorActivity.java
+++ Layouts/src/com/packtpub/layouts/LayoutSelectorActivity.java
@@ -1,15 +1,31 @@
 package com.packtpub.layouts;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class LayoutSelectorActivity extends Activity
+public class LayoutSelectorActivity extends ListActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        setListAdapter(new ArrayAdapter<String>(
+        		this,
+        		android.R.layout.simple_list_item_1,
+        		getResources().getStringArray(R.array.layouts)));
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+    	switch (position) {
+    	default:
+    		Toast.makeText(this, "Example not yet implemented", Toast.LENGTH_SHORT).show();
+    	}
     }
 }
