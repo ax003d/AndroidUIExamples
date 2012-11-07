478807e647c6184be8218f5c9c9e100af65e7ed0
Robert <rzheng@xtremeprog.com>
2012-11-07 16:02:28 +0800

    * start calculaotr

---
+++ Calculator/src/com/packtpub/calculator/CalculatorActivity.java
@@ -0,0 +1,15 @@
+package com.packtpub.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
